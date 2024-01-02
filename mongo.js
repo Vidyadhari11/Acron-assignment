a0f05c3f678c2015612d83f118eff6a224612665
Vidyadhari <vidyadharibelagal@gmail.com>
2024-01-02 14:00:48 +0530

    changes in css

--- mongo.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose")
-mongoose.connect("http://www.stackfow.com/2.3/collections")
-.then(()=>{console.log("mongodb connected")})
-.catch(()=>{console.log("failed")})
-
-const collection=mongoose.model("collection")
-
-module.exports=collection
